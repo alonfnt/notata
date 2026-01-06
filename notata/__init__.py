6727ef6a4a7ead9b9997d613f98ad6f13f73d19f
Albert Alonso <aalonso@proton.me>
2026-01-06 15:50:24 +0100

    chore: bump version to 0.2.1 with Python 3.10+ support and CI improvements

--- notata/__init__.py
+++ notata/__init__.py
@@ -2,7 +2,7 @@ from notata.logbook import Logbook
 from notata.experiment import Experiment
 from notata.reader import LogReader, ExperimentReader
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "Logbook",
